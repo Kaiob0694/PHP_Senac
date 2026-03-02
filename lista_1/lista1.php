7948f2642aa21d9a7cb57957dc50cb36dee071da
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-03-02 19:37:48 -0300

    feito em casa

--- lista_1/lista1.php
+++ lista_1/lista1.php
@@ -1,16 +1,36 @@
 <?php
-$nome = readline("Qual é seu nome? \n");
-echo "Olá " . $nome . PHP_EOL ;
-$cidade = readline("Em que cidade você mora? ") . PHP_EOL;
-$filme = readline("Qual é o seu filme favorito? ") . PHP_EOL;
-$genero = readline("Qual o genero do seu filme? ") . PHP_EOL;
-$duracao = readline("Digite a duração do seu Filme? ") . PHP_EOL;
-echo "Filme escolhido: " . $filme . PHP_EOL;
-echo "Genero do filme: " . $genero . PHP_EOL;
-echo "Duração: " . $duracao . PHP_EOL;
+
+$nome = readline("Qual é seu nome? ");
+echo "Olá $nome" . PHP_EOL;
+
+$cidade = readline("Em que cidade você mora? ");
+$filme = readline("Qual é o seu filme favorito? ");
+$genero = readline("Qual o gênero do seu filme? ");
+$duracao = readline("Digite a duração do seu filme (em minutos): ");
+
+echo PHP_EOL;
+echo "----- RESUMO -----" . PHP_EOL;
+echo "Cidade: $cidade" . PHP_EOL;
+echo "Filme escolhido: $filme" . PHP_EOL;
+echo "Gênero do filme: $genero" . PHP_EOL;
+echo "Duração: $duracao minutos" . PHP_EOL;
+
+echo PHP_EOL;
+$animal = readline("Digite seu animal animal favorito? ");
+$cor = readline("Digite sua cor favorito? ");
+
+echo PHP_EOL;
+echo "Seu animal favorito é $animal e sua cor favorita é $cor!";
+
+echo PHP_EOL;
+$user = readline("Digite seu usuario: ") . PHP_EOL;
+$descri = readline("Crie uma pequena descrição sobre sí: ") . PHP_EOL;
+echo PHP_EOL;
+
+echo "Cadastro relizado com SUCESSO!" . PHP_EOL;
+echo "UserName: $user" . PHP_EOL;
+echo "DESCRIÇÃO: " . PHP_EOL;
+echo "$descri"
 
 
 ?>
-
-
-
