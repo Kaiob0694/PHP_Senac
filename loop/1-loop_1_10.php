280aacda45a23cd9aaf7f6c63d9c33ca1f31fd08
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-03-11 10:51:04 -0300

    kaio

---
+++ loop/1-loop_1_10.php
@@ -0,0 +1,11 @@
+<?php
+
+echo"==== NUMERO DE 1 A 10 ====";
+
+echo PHP_EOL;
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "Número: $i \n";
+}
+
+?>
---
+++ loop/2-loop_10_1.php
@@ -0,0 +1,11 @@
+<?php
+
+echo"==== NUMERO DE 10 A 1 ====";
+
+echo PHP_EOL;
+
+for ($i = 10; $i >= 1; $i--) {
+    echo "Número: $i \n";
+}
+
+?>
