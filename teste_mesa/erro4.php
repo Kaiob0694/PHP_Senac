2b34b9898ca2b848627a66f40846c57ff61534f0
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-03-25 08:48:03 -0300

    kaio

---
+++ teste_mesa/erro4.php
@@ -0,0 +1,6 @@
+<?php
+$num1 = 10;
+$num2 = 5;
+$soma = $num1 + $num2; #primeiro erro soma não declarado como variavel
+echo "Resultado da soma: $soma";
+?>
---
+++ teste_mesa/soma.php
@@ -0,0 +1,13 @@
+<?php
+
+$soma = 0;
+
+for ($i = 1; $i <= 4; $i++) {
+
+$soma = $soma + $i;
+
+}
+
+echo $soma;
+
+?>
